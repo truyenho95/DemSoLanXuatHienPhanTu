607ad37e2438df34ca0ae70559a14998ab3c90c5
truyenho95 <truyenho.webdev@gmail.com>
2018-11-22 14:49:19 +0700

    Change script.js

--- script.js
+++ script.js
@@ -62,13 +62,13 @@ function insertSort(arr, n=arr.length)
   for(let i = 1; i<n; i++)
   {
     j = i-1;
-    t= arr[i];
-    while(j>=0 && t<arr[j])
+    temp = arr[i];
+    while(j>=0 && temp<arr[j])
     {
-      arr[j+1]=arr[j];
+      arr[j+1] = arr[j];
       j--;
     }
-    arr[j+1]=t;
+    arr[j+1] = temp;
   }
 }
 
